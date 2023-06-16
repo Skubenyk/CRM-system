71daf0ef0adf5fdd2f6ccb63688cffe4f0d21e62
Skubenyk <skubenikl@gmail.com>
2023-06-16 12:10:20 +0300

    Fixing an error after changing http to https

--- src/store/index.js
+++ src/store/index.js
@@ -21,7 +21,7 @@ export default createStore({
     async fetchCurrency() {
       const key = process.env.VUE_APP_FIXER;
       const res = await fetch(
-        `http://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,UAH`
+        `https://data.fixer.io/api/latest?access_key=${key}&symbols=USD,EUR,UAH`
       );
       return await res.json();
     },
